e80d3fa3cc54c0a3a922a36426ee57714099cb8a
eonofrey <38504767+eonofrey@users.noreply.github.com>
2019-01-29 17:08:10 -0500

    Create drake_rih_lyrics.R

---
+++ drake_rih_lyrics.R
@@ -0,0 +1,7 @@
+# Rap genius credentials
+client_id = "_AXQLV_eKSiCgXSy24FsXQZuGPK5OAYYzj1pNpF4O4u8qZUlMx2g8V2THZxDJlQx"
+client_secret = "l8aM2qmyr_kwCEoWdxWGcNVJ91Ya3dlyrSNB0VLf6rcIpcG9_8tt8ubALVg0kfRPoa5NfEw-uRH0YydZNbnfVQ"
+GENIUS_API_TOKEN = "n5TPUYeh9XP81ipo_Km9JsX4cLCT97zjxyh0xGB_iwqiNgmEP41c_dLLMFH8D2Xq"
+#rap genius token
+Sys.setenv(GENIUS_API_TOKEN = "n5TPUYeh9XP81ipo_Km9JsX4cLCT97zjxyh0xGB_iwqiNgmEP41c_dLLMFH8D2Xq")
+genius_token()
